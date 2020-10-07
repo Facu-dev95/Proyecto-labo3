8cf49156c03135effdd5f9ea8268e83d89821e0c
facudv <facu.j.benitez@gmail.com>
2020-10-07 16:09:59 -0300

    Se avanzó/modificó el html y se estableció conexión con API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo!")
+const titulo = 'hacker';
+const pagina = 2
+
+const url = `http://www.omdbapi.com/?s=${titulo}&pagina${pagina}&i=tt3896198&apikey=cb4fb574`;
+
+fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => console.log(resultado))
