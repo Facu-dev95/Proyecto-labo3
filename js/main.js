f02475f02a17846e6d6a79e61bf002ccecf05e19
facudv <facu.j.benitez@gmail.com>
2020-10-08 22:49:13 -0300

    Se obtiene limitadas peliculas aleatorias

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-const titulo = 'hacker';
-const pagina = 2
+let num;
+var data = [];
+var data2;
+var cont = 0;
+const max = 1999999;
+const min = 1000000;
 
-const url = `http://www.omdbapi.com/?s=${titulo}&pagina${pagina}&i=tt3896198&apikey=cb4fb574`;
 
-fetch(url)
+for(i=0; i<10; i++){
+
+    var cont2;
+
+    num = parseInt(Math.random() * (max - min) + min);
+    console.log(num);
+
+    let url = `http://www.omdbapi.com/?i=tt${num}&apikey=cb4fb574`;
+
+    fetch(url)
     .then(respuesta => respuesta.json())
-    .then(resultado => console.log(resultado))
+    .then(resultado => {
+
+        if(resultado.Type == "movie"){
+            data.push(resultado);
+            cont++;
+            return cont;
+        }
+        recogerDatos(data)
+    })
+
+}
+
+
+function recogerDatos(datos){
+    data2 = datos;
+    return data2;
+}
